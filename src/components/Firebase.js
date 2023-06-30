dcf1ef21e891423a18bbae25f35a4a6a9c622ec7
RohitSaklani <rohitsaklani1510@gmail.com>
2023-06-30 09:41:11 +0530

    slider fixed

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyCVqlDgc2aN-ob5C6gMFILbXIAUZSpjRwg",
-  authDomain: "netflix-clone-f62c3.firebaseapp.com",
-  projectId: "netflix-clone-f62c3",
-  storageBucket: "netflix-clone-f62c3.appspot.com",
-  messagingSenderId: "655701812399",
-  appId: "1:655701812399:web:0ebddb13eba046c60be05a",
-};
+const firebaseConfig = {};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
